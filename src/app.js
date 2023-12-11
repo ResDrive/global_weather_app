7ad93134adcb7eadff089fb591e5aafe38b4a6f8
ResDrive <36512134+ResDrive@users.noreply.github.com>
2023-12-11 22:58:06 +0200

    Added displaying API weather images

--- src/app.js
+++ src/app.js
@@ -20,23 +20,30 @@ function formatDate(date) {
 
 function displayReceivedWeather(response) {
   console.log(response.data);
-  let temperatureElement = document.querySelector("#city-temperature-value");
-  let temperature = response.data.temperature.current;
-  let cityElement = document.querySelector("#app-city-name");
-  let city = response.data.city;
-  let country = response.data.country;
-  let descriptionElement = document.querySelector("#description");
-  let humidityElement = document.querySelector("#humidity");
-  let windSpeedElement = document.querySelector("#wind-speed");
-  let timeElement = document.querySelector("#time");
-  let date = new Date(response.data.time * 1000);
+  if (response.data.city) {
+    let temperatureElement = document.querySelector("#city-temperature-value");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#app-city-name");
+    let city = response.data.city;
+    let country = response.data.country;
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector("#icon");
 
-  cityElement.innerHTML = `${city}, ${country}`;
-  timeElement.innerHTML = formatDate(date);
-  descriptionElement.innerHTML = response.data.condition.description;
-  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-  windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
-  temperatureElement.innerHTML = Math.round(temperature);
+    cityElement.innerHTML = `${city}, ${country}`;
+    timeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" alt=""
+                        class="app-city-temperature-icon">`;
+    temperatureElement.innerHTML = Math.round(temperature);
+  } else {
+    alert(`No data has been found. Please your refine search.`);
+  }
 }
 
 function searchCity(searchInputValue) {
