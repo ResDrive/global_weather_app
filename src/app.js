bf0cff605df1513ad4911969f4c4d8705d271fb4
ResDrive <36512134+ResDrive@users.noreply.github.com>
2023-12-23 15:58:51 +0200

    Added displaying forecast API data

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
-function formatDate(date) {
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
+function formatToShortDateName(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function formatToFullDateName(timestamp) {
+  let date = new Date(timestamp * 1000);
   let days = [
     "Sunday",
     "Monday",
@@ -10,12 +16,58 @@ function formatDate(date) {
     "Friday",
     "Saturday",
   ];
-  let day = days[date.getDay()];
+
+  return days[date.getDay()];
+}
+
+function formatToTime(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
+  return `${hours}:${minutes}`;
+}
 
-  return `${day} as of ${hours}:${minutes}`;
+function displayForecast(response) {
+  console.log(response.data);
+
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 6) {
+      forecastHtml =
+        forecastHtml +
+        `<div><div class="weather-forecast-date">${formatToShortDateName(
+          day.time
+        )}</div>
+        <div>
+         <img src="${
+           day.condition.icon_url
+         }" alt="" class="weather-forecast-icon">
+        </div>
+       <div class="weather-forecast-temperatures">
+            <span class="forecast-temperature-max">${Math.round(
+              day.temperature.maximum
+            )}°</span> <span
+            class="forecast-temperature-min">${Math.round(
+              day.temperature.minimum
+            )}°</span>
+        </div></div>`;
+    }
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
+function getForecast(responseDataCity) {
+  let apiKey = "a9d6234fb74f04etfce33ccaod0a2fee";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${responseDataCity}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayReceivedWeather(response) {
@@ -30,11 +82,12 @@ function displayReceivedWeather(response) {
     let humidityElement = document.querySelector("#humidity");
     let windSpeedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
-    let date = new Date(response.data.time * 1000);
     let iconElement = document.querySelector("#icon");
 
     cityElement.innerHTML = `${city}, ${country}`;
-    timeElement.innerHTML = formatDate(date);
+    timeElement.innerHTML = `${formatToFullDateName(
+      response.data.time
+    )} as of ${formatToTime(response.data.time)}, `;
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
@@ -62,46 +115,7 @@ function searchSubmitHandler(event) {
   searchCity(searchInput.value);
 }
 
-function getForecast(searchInputValue) {
-  let apiKey = "a9d6234fb74f04etfce33ccaod0a2fee";
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${searchInputValue}&key=${apiKey}&units=metric`;
-  console.log(apiUrl);
-
-  axios.get(apiUrl).then(displayForecast);
-}
-
-function displayForecast(response) {
-  console.log(response);
-
-  let daysMoc = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let forecastHtml = "";
-
-  daysMoc.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `<div><div class="weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon">🌞</div>
-       <div class="weather-forecast-temperatures">
-            <span class="forecast-temperature-max">18°</span> <span
-            class="forecast-temperature-min">12°</span>
-        </div></div>`;
-  });
-
-  let forecastElement = document.querySelector("#forecast");
-  forecastElement.innerHTML = forecastHtml;
-}
-
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmitHandler);
 
 searchCity("Kyiv");
-
-displayForecast();
